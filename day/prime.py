4d2a3f94b8fa42ea24198a1c78e4b432900b6695
georgreen-Manu <georgreen@localhost.localdomain>
2017-04-04 09:05:01 +0300

    added prime.py and testcases.py

---
+++ day/prime.py
@@ -0,0 +1,14 @@
+def prime_gen(n):
+    '''
+    input: n integer
+    output: k where k is the i th prime number in range (0, n)
+    '''
+    pass
+
+
+def prime_numbers(n):
+    '''
+    input: n integer
+    output: a list, containing prime numbers in range(0,n)
+    '''
+    pass
