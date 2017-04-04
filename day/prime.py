e80fce4334f08838a3b397badb50b363c57ed26f
georgreen-Manu <georgreen@localhost.localdomain>
2017-04-04 11:24:51 +0300

    added git commit and README file

--- day/prime.py
+++ day/prime.py
@@ -21,7 +21,15 @@ def prime_numbers(n):
     input: n integer
     output: a list, containing prime numbers in range(0,n)
     '''
-    pass
+    if type(n) != type(1):
+        return "Invalid Data Type"
+
+    solution = []
+    for i in range(0, n + 1):
+        if prime_test(i, solution):
+            solution.append(i)
+    return solution
+
 
 
 import unittest
@@ -70,6 +78,30 @@ class test_prime_test(unittest.TestCase):
 
 
 class test_prime_numbers(unittest.TestCase):
-    pass
+    '''
+    Unit test for prime_numbers
+    Test Strategy: egde cases : 0, 1
+                   special case : range(0,2)
+                   negtives ranges : any int that's negative range(0, negative_n)
+                   positive ranges : any int that's postive range(0, postive n)
+    coverage : selctive choose to cover major areas not full catesian
+    '''
+    def test_negative_range(self):
+        self.assertEqual([], prime_numbers(-17))
+        self.assertEqual([], prime_numbers(-1))
 
+    def test_postive_range(self):
+        self.assertEqual([2,3,5,7], prime_numbers(9))
+        self.assertEqual([2,3,5,7], prime_numbers(8))
+        self.assertEqual([2,3,5,7,11,13,17,19], prime_numbers(19))
+
+    def test_special_case(self):
+        self.assertEqual([2], prime_numbers(2))
+
+    def test_data_type(self):
+        self.assertEqual("Invalid Data Type", prime_numbers([]))
+
+    def test_edge_case(self):
+        self.assertEqual([], prime_numbers(0))
+        self.assertEqual([], prime_numbers(1))
 unittest.main()
