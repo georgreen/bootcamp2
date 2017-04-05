4c08ee2a770f688df57e0dc979d7aaec1d6a47e6
georgreen-Manu <georgreen@localhost.localdomain>
2017-04-05 18:23:44 +0300

    update missing_number efficiency

--- day2/lab/missing_number.py
+++ day2/lab/missing_number.py
@@ -5,22 +5,22 @@ def find_missing(list_1, list_2):
     input -> two lists
     output -> list_2[index] != list_1[index]
     '''
+        
     hashTable = {}
     hashTable_2 = {}
-    #insert everything in hashTable(elminates any duplicates)
-    for elem in list_1:
-        if elem not in hashTable:
-            hashTable[elem] = elem
-
-    for elem in list_2:
-        if elem not in hashTable_2:
-            hashTable_2[elem] = elem
+    #insert everything in list_1 into hashTable(elminates any duplicates)
+    hashTable = { x : x for x in list_1 if x not in hashTable}
+    
+    #insert everything in list_2 into hashTable_2(eliminates any duplicates)
+    hashTable_2 = { x : x for x in list_2 if x not in hashTable_2}
+    
+    #make sure hashTable is the largest dictionary
+    if len(hashTable_2) > len(hashTable):
+        hashTable, hashTable_2 = (hashTable_2, hashTable)
+        
     #search for elements in hashTable that are missing in hashTable_2
     for elem in hashTable:
         if elem not in hashTable_2:
             return elem
-    #search hashTable for elem in hashTable_2 :return any missing
-    for elem in hashTable_2:
-        if elem not in hashTable:
-            return elem
+            
     return 0
