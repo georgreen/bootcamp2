80f36001d32bcbf9885cf2616ce173b2c624387d
georgreen-Manu <georgreen@localhost.localdomain>
2017-04-05 10:44:43 +0300

    Added function documentation

--- day1/labs/max_min.py
+++ day1/labs/max_min.py
@@ -1,9 +1,13 @@
 def find_max_min(list_of_integers):
+  '''
+  input: list of ints
+  output: if min != max list[min_element, max_element] else [len(list)]
+  '''
   sorted_list = sorted(list_of_integers)
   min_ele = sorted_list[0]
   max_ele = sorted_list[len(sorted_list) - 1]
 
   if(min_ele == max_ele):
-    return [min_ele]
+    return [len(list_of_integers)]
   else:
     return [min_ele,max_ele]
--- day1/labs/wordcount.py
+++ day1/labs/wordcount.py
@@ -1,7 +1,10 @@
 def words(string_input):
+  '''
+  input : string
+  output: dict{word : count}
+  '''
   results = {}
   split_words = string_input.split()
-  seen_words = []
 
   for word in split_words:
       try:
@@ -10,9 +13,8 @@ def words(string_input):
       except ValueError:
           pass
 
-      if seen_words.__contains__(word):
-          results[word] = results[word] + 1
+      if word in results:
+          results[word] += 1
       else:
-          seen_words.append(word)
           results[word] = 1
   return results
